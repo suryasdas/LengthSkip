ff363671c3819894513dadb1123b48cc9313dc1b
suryasdas <47587516+suryasdas@users.noreply.github.com>
2021-01-19 18:12:56 +0530

    Initiation

---
+++ project-map/project-map/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project-map
+//
+//  Created by Surya Das on 1/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project-map/project-map/project_mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_mapApp.swift
+//  project-map
+//
+//  Created by Surya Das on 1/19/21.
+//
+
+import SwiftUI
+
+@main
+struct project_mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
