ed728cec620884f75868ddfca09a5cc09b56419f
suryasdas <47587516+suryasdas@users.noreply.github.com>
2021-01-21 06:40:40 +0530

    no message

--- project-map/project-map/ViewController.swift
+++ project-map/project-map/ViewController.swift
@@ -10,79 +10,33 @@ import MapKit
 import CoreLocation
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
-
-    //MARK:- Variable Declarations
-
-    @IBOutlet weak var mMapView: MKMapView!
-    var locationManager:CLLocationManager!
-    var currentLocationStr = "Current location"
-
-    //MARK:- ViewController LifeCycle Methods
+    
+    @IBOutlet weak var map: MKMapView!
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
 
-    override func viewDidAppear(_ animated: Bool) {
-        determineCurrentLocation()
-    }
-
-    //MARK:- CLLocationManagerDelegate Methods
-
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let mUserLocation:CLLocation = locations[0] as CLLocation
-
-        let center = CLLocationCoordinate2D(latitude: mUserLocation.coordinate.latitude, longitude: mUserLocation.coordinate.longitude)
-        let mRegion = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-
-        mMapView.setRegion(mRegion, animated: true)
-    }
-func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Error - locationManager: \(error.localizedDescription)")
-    }
-//MARK:- Intance Methods
-
-func determineCurrentLocation() {
-    locationManager = CLLocationManager()
-    locationManager.delegate = self
-    locationManager.desiredAccuracy = kCLLocationAccuracyBest
-    locationManager.requestAlwaysAuthorization()
-
-    if CLLocationManager.locationServicesEnabled() {
-        locationManager.startUpdatingLocation()
-    }
-
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let mUserLocation:CLLocation = locations[0] as CLLocation
-        let center = CLLocationCoordinate2D(latitude: mUserLocation.coordinate.latitude, longitude: mUserLocation.coordinate.longitude)
-        let mRegion = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        mMapView.setRegion(mRegion, animated: true)
-
-        // Get user's Current Location and Drop a pin
-    let mkAnnotation: MKPointAnnotation = MKPointAnnotation()
-        mkAnnotation.coordinate = CLLocationCoordinate2DMake(mUserLocation.coordinate.latitude, mUserLocation.coordinate.longitude)
-        mkAnnotation.title = self.setUsersClosestLocation(mLatitude: mUserLocation.coordinate.latitude, mLongitude: mUserLocation.coordinate.longitude)
-        mMapView.addAnnotation(mkAnnotation)
-    }
-    //MARK:- Intance Methods
-
-    func setUsersClosestLocation(mLattitude: CLLocationDegrees, mLongitude: CLLocationDegrees) -> String {
-        let geoCoder = CLGeocoder()
-        let location = CLLocation(latitude: mLattitude, longitude: mLongitude)
-
-        geoCoder.reverseGeocodeLocation(location) {
-            (placemarks, error) -> Void in
-
-            if let mPlacemark = placemarks{
-                if let dict = mPlacemark[0].addressDictionary as? [String: Any]{
-                    if let Name = dict["Name"] as? String{
-                        if let City = dict["City"] as? String{
-                            self.currentLocationStr = Name + ", " + City
-                        }
-                    }
-                }
-            }
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
         }
-        return currentLocationStr
     }
-}}
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last{
+            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            self.map.setRegion(region, animated: true)
+        }
+    }
+}
+
+
+
+
+    
