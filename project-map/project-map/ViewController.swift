f60aee83766a1cfcfcf6b507341d620b58450348
suryasdas <47587516+suryasdas@users.noreply.github.com>
2021-01-21 06:12:09 +0530

    no message

--- project-map/project-map/ViewController.swift
+++ project-map/project-map/ViewController.swift
@@ -7,15 +7,48 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, CLLocationManagerDelegate,MKMapViewDelegate {
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      super.viewDidLoad()
+      checkLocationServices()
     }
-
-
+    
+    func checkAuthorizationStatus() {
+      switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse: break
+        case .denied: break
+        case .notDetermined: break
+        case .restricted: break
+        case .authorizedAlways: break
+      }
+    }
+    
+    func checkLocationServices() {
+      if CLLocationManager.locationServicesEnabled() {
+        checkLocationAuthorization()
+      } else {
+        // Show alert letting the user know they have to turn this on.
+      }
+    }
+    
+    func checkLocationAuthorization() {
+      switch CLLocationManager.authorizationStatus() {
+      case .authorizedWhenInUse:
+        mapView.showsUserLocation = true
+       case .denied: // Show alert telling users how to turn on permissions
+       break
+      case .notDetermined:
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+      case .restricted: // Show an alert letting them know what’s up
+       break
+      case .authorizedAlways:
+       break
+      }
+    }
 }
-
